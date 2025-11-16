212215a150e895997cb927032d5faa415541baa1
Alice Norwood <willingtodev@gmail.com>
2025-11-15 23:59:55 -0500

    simple bytecode op execution

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "chunk.h"
+#include "common.h"
+#include "debug.h"
+
+int main(int argc, char **argv) {
+  Chunk chunk;
+  initChunck(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
+  return 0;
+}
