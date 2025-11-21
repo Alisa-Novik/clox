1f9be994df5513b1bcfc10281b77b6fff97ee394
Alice Norwood <willingtodev@gmail.com>
2025-11-21 09:23:05 -0500

    local variables

--- chunk.h
+++ chunk.h
@@ -16,6 +16,8 @@ typedef enum {
   OP_RETURN,
   OP_ADD,
   OP_POP,
+  OP_GET_LOCAL,
+  OP_SET_LOCAL,
   OP_GET_GLOBAL,
   OP_SET_GLOBAL,
   OP_DEFINE_GLOBAL,
--- debug.c
+++ debug.c
@@ -11,6 +11,12 @@ void disassembleChunk(Chunk *chunk, const char *name) {
   }
 }
 
+static int byteInstruction(const char *name, Chunk *chunk, int offset) {
+  uint8_t slot = chunk->code[offset + 1];
+  printf("%-16s %4d\n", name, slot);
+  return offset + 2;
+}
+
 static int simpleInstruction(const char *name, int offset) {
   printf("%s\n", name);
   return offset + 1;
@@ -46,6 +52,10 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     return simpleConstant("OP_DEFINE_GLOBAL", chunk, offset);
   case OP_POP:
     return simpleInstruction("OP_POP", offset);
+  case OP_GET_LOCAL:
+    return byteInstruction("OP_GET_LOCAL", chunk, offset);
+  case OP_SET_LOCAL:
+    return byteInstruction("OP_SET_LOCAL", chunk, offset);
   case OP_ADD:
     return simpleInstruction("OP_ADD", offset);
   case OP_SUBTRACT:
