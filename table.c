3bea3352e52a5b39caa77220c3785572ade95efe
Alice Norwood <willingtodev@gmail.com>
2025-11-23 15:33:08 -0500

    faster hash table probing

--- table.c
+++ table.c
@@ -21,7 +21,7 @@ void freeTable(Table *table) {
 }
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & (capacity - 1);
   Entry *tombstone = NULL;
   for (;;) {
     Entry *entry = &entries[index];
@@ -36,7 +36,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
     } else if (entry->key == key) {
       return entry;
     }
-    index = (index + 1) % capacity;
+    index = (index + 1) & (capacity - 1);
   }
 }
 
@@ -69,7 +69,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
                            uint32_t hash) {
   if (table->count == 0)
     return NULL;
-  uint32_t index = hash % table->capacity;
+  uint32_t index = hash & (table->capacity - 1);
   for (;;) {
     Entry *entry = &table->entries[index];
     if (entry->key == NULL) {
@@ -80,7 +80,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
       return entry->key;
     }
 
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & (table->capacity - 1);
   }
 }
 
